8dc323263b74e1970c84cb132ddf6f8f1b7126d4
Luiz-Felipe-Dorneles <dornelesluizfelipe1@gmail.com>
2025-05-13 10:38:05 -0300

    upddatabase

--- data/must_watch.sql
+++ data/must_watch.sql
@@ -1,3 +1,183 @@
-create database if not exists `must_watch`;
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: must_watch
+-- ------------------------------------------------------
+-- Server version	8.0.35
 
-use `much_watch`
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `ator`
+--
+
+DROP TABLE IF EXISTS `ator`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ator` (
+  `id_ator` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) NOT NULL,
+  PRIMARY KEY (`id_ator`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ator`
+--
+
+LOCK TABLES `ator` WRITE;
+/*!40000 ALTER TABLE `ator` DISABLE KEYS */;
+INSERT INTO `ator` VALUES (1,'Millie Bobby Brown');
+/*!40000 ALTER TABLE `ator` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ator_serie`
+--
+
+DROP TABLE IF EXISTS `ator_serie`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ator_serie` (
+  `id_ator` int NOT NULL,
+  `id_serie` int NOT NULL,
+  `personagem` varchar(100) NOT NULL,
+  PRIMARY KEY (`id_ator`,`id_serie`),
+  KEY `id_serie_idx` (`id_serie`),
+  CONSTRAINT `id_autor` FOREIGN KEY (`id_ator`) REFERENCES `ator` (`id_ator`),
+  CONSTRAINT `id_serie` FOREIGN KEY (`id_serie`) REFERENCES `serie` (`id_serie`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ator_serie`
+--
+
+LOCK TABLES `ator_serie` WRITE;
+/*!40000 ALTER TABLE `ator_serie` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ator_serie` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `avaliacao_serie`
+--
+
+DROP TABLE IF EXISTS `avaliacao_serie`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `avaliacao_serie` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_serie` int DEFAULT NULL,
+  `nota` int NOT NULL,
+  `comentario` text NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_id_serie_idx` (`id_serie`),
+  CONSTRAINT `fk_id_serie` FOREIGN KEY (`id_serie`) REFERENCES `serie` (`id_serie`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `avaliacao_serie`
+--
+
+LOCK TABLES `avaliacao_serie` WRITE;
+/*!40000 ALTER TABLE `avaliacao_serie` DISABLE KEYS */;
+/*!40000 ALTER TABLE `avaliacao_serie` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `categoria`
+--
+
+DROP TABLE IF EXISTS `categoria`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `categoria` (
+  `id_categoria` int NOT NULL AUTO_INCREMENT,
+  `nome_categoria` varchar(50) NOT NULL,
+  PRIMARY KEY (`id_categoria`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `categoria`
+--
+
+LOCK TABLES `categoria` WRITE;
+/*!40000 ALTER TABLE `categoria` DISABLE KEYS */;
+INSERT INTO `categoria` VALUES (1,'Ação');
+/*!40000 ALTER TABLE `categoria` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `motivo_assistir`
+--
+
+DROP TABLE IF EXISTS `motivo_assistir`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `motivo_assistir` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_serie` int DEFAULT NULL,
+  `motivo` text NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_serie_idx` (`id_serie`),
+  CONSTRAINT `fk_serie_id` FOREIGN KEY (`id_serie`) REFERENCES `serie` (`id_serie`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `motivo_assistir`
+--
+
+LOCK TABLES `motivo_assistir` WRITE;
+/*!40000 ALTER TABLE `motivo_assistir` DISABLE KEYS */;
+/*!40000 ALTER TABLE `motivo_assistir` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `serie`
+--
+
+DROP TABLE IF EXISTS `serie`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `serie` (
+  `id_serie` int NOT NULL AUTO_INCREMENT,
+  `titulo` varchar(100) NOT NULL,
+  `descricao` text NOT NULL,
+  `ano_lancamento` int NOT NULL,
+  `nome_categoria` varchar(70) NOT NULL,
+  PRIMARY KEY (`id_serie`),
+  KEY `categoria_nome_idx` (`nome_categoria`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `serie`
+--
+
+LOCK TABLES `serie` WRITE;
+/*!40000 ALTER TABLE `serie` DISABLE KEYS */;
+INSERT INTO `serie` VALUES (2,'Stranger Things','Na década de 1980, um grupo de amigos se envolve em uma série de eventos sobrenaturais na pacata cidade de Hawkins. Eles enfrentam criaturas monstruosas, agências secretas do governo e se aventuram em dimensões paralelas.',2016,'Ação'),(3,'Prison Break','Michael Scofield é um homem desesperado em uma situação desesperada. Seu irmão, Lincoln Burrows, foi condenado por um crime que não cometeu e colocado no corredor da morte. Michael rende um banco para conseguir ser encarcerado junto ao irmão na penitenciária estadual de Fox River e coloca em ação uma série de planos elaborados para permitir a fuga de Lincoln e provar sua inocência. Mesmo fora da prisão, seus perigos não acabam -- os irmãos precisam fugir para evitar a recaptura e lutar contra uma intricada conspiração policial que coloca a vida de todo mundo em risco.',2017,'Ação');
+/*!40000 ALTER TABLE `serie` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-05-13  8:27:41
